28ac94ee98cc858d4d9d4904dd8183d23d116026
NastyaLo <loyko2802@gmail.com>
2018-09-13 17:29:09 +0300

    Determine the minimum number of coins needed to make change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 0) return {};
+    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    let reminder = currency;
+    let coinCounter = { 0:0, 1:0, 2:0, 3:0, 4:0 };
+
+    let exchangeArr = [ 50, 25, 10, 5, 1 ];
+
+    for (let i = 0; i < exchangeArr.length; i++) {
+        if(reminder == 0) break;
+        coinCounter[i] = Math.floor(reminder/exchangeArr[i]);
+        reminder -= coinCounter[i]*exchangeArr[i];
+    };
+
+    var result = {
+        "H":coinCounter[0],
+        "Q":coinCounter[1],
+        "D":coinCounter[2],
+        "N":coinCounter[3],
+        "P":coinCounter[4]
+    };
+
+    for (var key in result) {
+        if(result[key] == 0) delete result[key];
+    };
+
+    return result;
 }
